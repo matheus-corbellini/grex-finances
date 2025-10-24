3e94479c678d3a91a6266b12cdeb66f388a729b2
Nicolas Tresoldi <nicolastresoldi@gmail.com>
2025-10-24 15:55:20 -0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { auth } from "../firebaseConfig";
+import { auth } from "../../firebaseConfig";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://grex-finances.onrender.com",
 });
 
 // Interceptor para adicionar token do Firebase em todas as requisições
